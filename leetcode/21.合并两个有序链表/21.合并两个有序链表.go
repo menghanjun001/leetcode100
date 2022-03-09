fa20296e8603bdbfe514282732f2a790ec37f668
menghanjun <406228554@qq.com>
2022-03-10 00:18:04 +0800

    no message

---
+++ leetcode/21.合并两个有序链表/21.合并两个有序链表.go
@@ -0,0 +1,25 @@
+package leetcode
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+//https://leetcode-cn.com/problems/merge-two-sorted-lists/solution/
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	//0.边界条件
+	if list1 == nil {
+		return list2
+	}
+	if list2 == nil {
+		return list1
+	}
+	//1.递归合并
+	if list1.Val < list2.Val {
+		list1.Next = mergeTwoLists(list1.Next, list2)
+		return list1
+	} else {
+		list2.Next = mergeTwoLists(list1, list2.Next)
+		return list2
+	}
+}
